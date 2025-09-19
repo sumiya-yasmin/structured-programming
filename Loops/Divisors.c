103b94f471af1e7664cfa64a68b37f66824bf87a
sumiya-yasmin <sumiyayasmin665@gmail.com>
2025-09-19 23:58:06 +0600

    Solve divisor problem

---
+++ Loops/Divisors.c
@@ -0,0 +1,13 @@
+//Given a number N. Print all the divisors of N in ascending order.
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++){
+        if(n%i==0){
+            printf("%d\n",i);
+        }
+    }
+    return 0;
+}
