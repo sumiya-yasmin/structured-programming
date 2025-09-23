ad204f019e541e4072a86ec91f30dc3d48604d13
sumiya-yasmin <sumiyayasmin665@gmail.com>
2025-09-23 16:30:02 +0600

    solve count problem

---
+++ Strings/Count.c
@@ -0,0 +1,14 @@
+//Given a string S. Print the summation of its digits.
+#include<stdio.h>
+int main()
+{
+    char s[1000001];
+    scanf("%s",s);
+    int length= strlen(s);
+    int sum=0;
+    for(int i=0;i<length;i++){
+        sum+=s[i]-'0';
+    }
+    printf("%d", sum);
+    return 0;
+}
