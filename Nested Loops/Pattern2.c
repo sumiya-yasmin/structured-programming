e0fc9784018e96f1592b2510f46200d1e01c3cc8
sumiya-yasmin <sumiyayasmin665@gmail.com>
2025-10-11 22:59:11 +0600

    Add patterns

---
+++ Nested Loops/Pattern2.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    int star = 2*n-1;
+    int space = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= space; j++)
+        {
+            printf(" ");
+        }
+        for (int j = 1; j <= star; j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+        star-=2;
+        space++;
+
+    }
+    return 0;
+}
